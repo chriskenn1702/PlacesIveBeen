6ef054748217dcaa420484b5f6b1e05efa52ed7c
Christopher Kennedy <kennednu@bc.edu>
2023-03-27 16:42:39 -0400

    Initial Commit

---
+++ PlacesIveBeen/PlacesIveBeenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesIveBeenApp.swift
+//  PlacesIveBeen
+//
+//  Created by Christopher Kennedy on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct PlacesIveBeenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
